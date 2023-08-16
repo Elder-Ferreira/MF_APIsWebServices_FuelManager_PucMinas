7adae263a39fe643674255c07609d7046133fa8c
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-08-15 22:48:17 -0300

    Criando models veículo e consumos

---
+++ mf-apis-web-services-fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public string Placa { get; set; }
+
+        [Required]
+        public int AnoFabricacao { get; set; }
+
+        [Required]
+        public int AnoModelo { get; set; }
+
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
