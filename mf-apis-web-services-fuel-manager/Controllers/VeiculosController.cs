be32c9a7d2030c646b46acbd6e7ee72037804566
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-08-18 17:25:01 -0300

    Criação do Controller Veiculos e método Get

---
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -0,0 +1,26 @@
+﻿using mf_apis_web_services_fuel_manager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Veiculos.ToListAsync();
+            return Ok(model);
+        }
+    }
+}
